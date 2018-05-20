e99e4f96f5ae1a7147762099210c690c6ff810d5
Coenie <coenie.basson@gmail.com>
2018-05-20 23:26:56 +0100

    Couple of days' work

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private int currentLevel = 0;
+
+    void Awake() {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Current Level: " + currentLevel);
+        if (currentLevel == 0) {
+            Invoke("LoadNextLevel", 5);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LoadNextLevel() {
+        currentLevel = currentLevel + 1;
+        Debug.Log("Loading level: "+currentLevel);
+        SceneManager.LoadScene(currentLevel);
+    }
+}
